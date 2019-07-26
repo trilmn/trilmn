9b55a304f86e70d3c1f9269cecee837e3fc011ef
Tri Nguyen <tringuyen@anduintransact.com>
2019-07-26 20:45:49 +0700

    Add CV pdf

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,20 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header className="flex items-center justify-between mb-12 pl-4 pr-4">
+  <header className="flex mb-12 pl-4 pr-4 items-center justify-between">
     <Link to="/" className="flex items-center justify-start transition-fast hover:opacity-50">
       <img src= "/logo.png" alt="Logo" className="w-12 h-12"/>
       <div className="ml-4">
         <h1 className="text-base font-bold">
           {siteTitle}
         </h1>
-        <span className="text-sm text-gray-600">minhtri247@gmail.com</span>
+        <span className="text-sm text-gray-600">tri@trilmn.com</span>
       </div>
     </Link> 
     <ul className="flex items-center">
+      <li className="lg:mr-6 mr-4">
+        <a href="/tringuyen-cv.pdf" title="View my CV →" target="_blank" className="block transition-fast hover:opacity-50"><img src= "/icon/document.svg" alt="Icon Document" className=""/></a>
+      </li>
       <li className="lg:mr-6 mr-4">
         <a href="https://dribbble.com/minhtri247" title="View my dribbble shots →" target="_blank" className="block transition-fast hover:opacity-50"><img src= "/icon/dribbble.svg" alt="Icon Dribbble" className=""/></a>
       </li>
